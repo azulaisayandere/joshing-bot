6672a97b67439b0f1d5a326997fa378a8c028669
max <theoriginalderp4@gmail.com>
2022-02-25 14:49:09 -0800

    started on play command

--- bot_cmds/bot_commands.py
+++ bot_cmds/bot_commands.py
@@ -7,6 +7,11 @@ from logs.logs import userlist, write_user
 # Establish client user
 client = commands.Bot(command_prefix="josh ")
 
+# i hate rewriting this every time
+async def typing(ctx, x):
+    await ctx.channel.trigger_typing()
+    await sleep(x)
+
 # Discord commands
 @client.command()
 async def stats(ctx, name):
@@ -14,30 +19,33 @@ async def stats(ctx, name):
         if name == "me":
             for user in userlist:
                 if ctx.author.id == user['uid']:
-                    await ctx.channel.trigger_typing()
-                    await sleep(2)
+                    await typing(ctx, 2)
                     await ctx.channel.send(f"Josh Stats for {user['name']}, Message Count: {user['cnt']}, Rate: 1/{user['dnm']}")
         else:
             for user in userlist:
                 if name == user['name']:
-                    await ctx.channel.trigger_typing()
-                    await sleep(2)
+                    await typing(ctx, 2)
                     await ctx.channel.send(f"Josh Stats for {user['name']}, Message Count: {user['cnt']}, Rate: 1/{user['dnm']}")
     except Forbidden:
         print(f"[{ctx.message.created_at.strftime('%H:%M:%S')}] Forbidden 403 Encountered")
 
 @client.command()
 async def rate(ctx, user, ndnm):
-    if ctx.author.id == 204366690446737419:
-        for victim in userlist:
-            if user == victim['name']:
-                victim['dnm'] = int(ndnm)
-                await ctx.channel.trigger_typing()
-                await sleep(2)
-                await ctx.channel.send(f"Joshing rate for {victim['name']}: 1/{victim['dnm']}")
-                with open('user_log.json', 'w') as userfile:
-                    dump(write_user, userfile, indent=2)
-    else:
-        await ctx.channel.trigger_typing()
-        await sleep(1)
-        await ctx.channel.send("you're not my master fuck off")
+    try:
+        if ctx.author.id == 204366690446737419:
+            for victim in userlist:
+                if user == victim['name']:
+                    victim['dnm'] = int(ndnm)
+                    await typing(ctx, 2)
+                    await ctx.channel.send(f"Joshing rate for {victim['name']}: 1/{victim['dnm']}")
+                    with open('user_log.json', 'w') as userfile:
+                        dump(write_user, userfile, indent=2)
+        else:
+            await typing(ctx, 1)
+            await ctx.channel.send("you're not my master fuck off")
+    except Forbidden:
+        print(f"[{ctx.message.created_at.strftime('%H:%M:%S')}] Forbidden 403 Encountered")
+
+@client.command()
+async def play(ctx, url):
+    pass
