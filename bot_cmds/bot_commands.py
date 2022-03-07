9f6d0ad16f4693aa18ed0059d975b470fee28c0d
max <theoriginalderp4@gmail.com>
2022-03-07 15:48:17 -0800

    updated commands, percentages! tags!

--- bot_cmds/bot_commands.py
+++ bot_cmds/bot_commands.py
@@ -20,12 +20,12 @@ async def stats(ctx, name):
             for user in userlist:
                 if ctx.author.id == user['uid']:
                     await typing(ctx, 2)
-                    await ctx.channel.send(f"Josh Stats for {user['name']}, Message Count: {user['cnt']}, Rate: 1/{user['dnm']}")
+                    await ctx.channel.send(f"Josh Stats for {user['name']}, Message Count: {user['cnt']}, Rate: {round((100/user['dnm']), 1)}%")
         else:
             for user in userlist:
                 if (name == user['name']) or (name == f"<@!{user['uid']}>"):
                     await typing(ctx, 2)
-                    await ctx.channel.send(f"Josh Stats for {user['name']}, Message Count: {user['cnt']}, Rate: 1/{user['dnm']}")
+                    await ctx.channel.send(f"Josh Stats for {user['name']}, Message Count: {user['cnt']}, Rate: {round((100/user['dnm']), 1)}%")
     except Forbidden:
         print(f"[{ctx.message.created_at.strftime('%H:%M:%S')}] Forbidden 403 Encountered")
 
@@ -34,12 +34,12 @@ async def rate(ctx, user, ndnm):
     try:
         if ctx.author.id == 204366690446737419:
             for victim in userlist:
-                if user == victim['name']:
+                if (user == victim['name']) or (user == f"<@!{victim['uid']}>"):
                     victim['dnm'] = int(ndnm)
                     await typing(ctx, 2)
                     with open('user_log.json', 'w') as userfile:
                         dump(write_user, userfile, indent=2)
-                    await ctx.channel.send(f"Joshing rate for {victim['name']}: 1/{victim['dnm']}")
+                    await ctx.channel.send(f"Joshing rate for {victim['name']}: {round((100/victim['dnm']), 1)}%")
         else:
             await typing(ctx, 1)
             await ctx.channel.send("you're not my master fuck off")
